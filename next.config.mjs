2b90819f72d6c400f54eea7a70d492bf46dfd257
AbhishekR14 <aranjan0288@gmail.com>
2024-06-21 19:54:32 +0530

    removed gitpages addition

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-
-  // Add basePath
-  basePath: "/github-pages",
-  // Exclude dynamic API routes from export
-  // Implement generateStaticParams for dynamic API routes
-  generateStaticParams: async () => {
-    // Define static paths and their parameters
-    return {
-      "/": { page: "/" },
-      // Add other static routes here as needed
-    };
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
