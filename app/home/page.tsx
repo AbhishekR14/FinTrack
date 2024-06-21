7c46ba3cf602ae4e41ffb3aa0453bf96520ac132
AbhishekR14 <aranjan0288@gmail.com>
2024-06-21 18:55:56 +0530

    test if deplo starts

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,5 +4,5 @@ import { useSession } from "next-auth/react";
 
 export default function Home() {
   const session = useSession();
-  return <div className="text-white">Hi:{JSON.stringify(session)}</div>;
+  return <div className="text-white">Hi test:{JSON.stringify(session)}</div>;
 }
