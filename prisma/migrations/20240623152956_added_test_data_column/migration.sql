2b924282bee6186dfb976282a1a6cce0fcde653f
AbhishekR14 <aranjan0288@gmail.com>
2024-06-23 21:08:22 +0530

    Database schema changes

---
+++ prisma/migrations/20240623152956_added_test_data_column/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Transcations" ADD COLUMN     "testdata" BOOLEAN NOT NULL DEFAULT false;
---
+++ prisma/migrations/20240623153803_added_deafult_date_time/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Transcations" ALTER COLUMN "date" SET DEFAULT CURRENT_TIMESTAMP;
