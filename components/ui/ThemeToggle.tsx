e7b2f3b6385a6225e629493345ccc43489322eb6
AbhishekR14 <aranjan0288@gmail.com>
2024-06-28 16:34:59 +0530

    Removed system from theme

--- components/ui/ThemeToggle.tsx
+++ components/ui/ThemeToggle.tsx
@@ -75,19 +75,6 @@ export function ThemeToggle() {
               </a>
             </li>
           </ul>
-          <ul className="py-2" aria-labelledby="user-menu-button">
-            <li>
-              <a
-                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                onClick={() => {
-                  setTheme("system");
-                  toggleDropdown();
-                }}
-              >
-                System
-              </a>
-            </li>
-          </ul>
         </div>
       )}
     </div>
