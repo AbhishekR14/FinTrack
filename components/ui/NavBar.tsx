93e95df10b274b350028aac2e595133bbc3529c6
AbhishekR14 <aranjan0288@gmail.com>
2024-07-13 00:21:19 +0530

    MOV

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -172,19 +172,28 @@ export default function NavBar(props: navBarProps) {
                     </SelectContent>
                   </Select>
                 </div>
-                <ul className="py-2" aria-labelledby="user-menu-button">
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white rounded-md"
-                      onClick={() => {
-                        signOut();
-                      }}
-                    >
-                      Sign out
-                    </a>
-                  </li>
-                </ul>
+                {isSelectOpen ? <ul className="py-2" aria-labelledby="user-menu-button">
+                    <li>
+                      <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white rounded-md"                        
+                      >
+                        Sign out
+                      </a>
+                    </li>
+                  </ul> : <ul className="py-2" aria-labelledby="user-menu-button">
+                    <li>
+                      <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white rounded-md"
+                        onClick={() => {
+                          signOut();
+                        }}
+                      >
+                        Sign out
+                      </a>
+                    </li>
+                  </ul>}
               </div>
             )}
           </div>
