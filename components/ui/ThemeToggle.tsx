201e4673fdd567856771a5ced253df34236f24d7
AbhishekR14 <aranjan0288@gmail.com>
2024-07-14 13:16:14 +0530

    Change Theme Easily

--- components/ui/ThemeToggle.tsx
+++ components/ui/ThemeToggle.tsx
@@ -6,77 +6,25 @@ import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme();
-  const [isDropdownOpen, setIsDropdownOpen] = React.useState(false);
-  const dropdownRef = React.useRef<HTMLDivElement>(null);
-
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
-
-  const handleClickOutside = (event: MouseEvent) => {
-    if (
-      dropdownRef.current &&
-      !dropdownRef.current.contains(event.target as Node)
-    ) {
-      setIsDropdownOpen(false);
-    }
-  };
-
-  React.useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
+  const theme = useTheme();
 
   return (
-    <div className="relative" ref={dropdownRef}>
+    <div className="relative">
       <Button
         variant="outline"
         size="icon"
-        aria-expanded={isDropdownOpen}
-        onClick={toggleDropdown}
+        onClick={() => {
+          if (theme.theme === "light") {
+            theme.setTheme("dark");
+          } else {
+            theme.setTheme("light");
+          }
+        }}
       >
         <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
         <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         <span className="sr-only">Toggle theme</span>
       </Button>
-
-      {isDropdownOpen && (
-        <div
-          className="absolute right-0 z-50 mt-2 w-24 bg-white rounded-md shadow-lg dark:bg-gray-700"
-          id="user-dropdown"
-        >
-          <ul className="py-2" aria-labelledby="user-menu-button">
-            <li>
-              <a
-                className="block  px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                onClick={() => {
-                  setTheme("light");
-                  toggleDropdown();
-                }}
-              >
-                Light
-              </a>
-            </li>
-          </ul>
-
-          <ul className="py-2" aria-labelledby="user-menu-button">
-            <li>
-              <a
-                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                onClick={() => {
-                  setTheme("dark");
-                  toggleDropdown();
-                }}
-              >
-                Dark
-              </a>
-            </li>
-          </ul>
-        </div>
-      )}
     </div>
   );
 }
